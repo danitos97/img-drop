e2a81b7ea7f7e5833e0d2a069307b0847914e000
Daniel Rivera <daniel_doni@hotmail.com>
2021-12-30 16:03:05 -0600

    drop events UX

--- img-drop.js
+++ img-drop.js
@@ -6,35 +6,55 @@
 // Array.prototype.on = function(ev,f){this.forEach(e=>{e.on(ev,f)})}
 // const create = tag=>document.createElement(tag);
 
-class DragDrop{
+class ImgDrop{
 
     constructor(div){
 
         const dropArea = document.createElement('div')
-        dropArea.classList.add('drop-area')
+        dropArea.classList.add('drop-area','droppable')
         dropArea.addEventListener('click',()=> console.log('click drop'))
         
         const dropH2 = document.createElement('h2')
-        dropH2.innerHTML = 'Arrastra aquí las imagenes de la nota.'
+        dropH2.innerHTML = 'Arrastra aquí las imagenes'
+        dropH2.classList.add('droppable')
         dropArea.appendChild(dropH2);
 
         const dropH3 = document.createElement('h3')
         dropH3.innerHTML = 'También puedes seleccionarlas'
+        dropH3.classList.add('droppable')
         dropArea.appendChild(dropH3)
 
         const input = document.createElement('input')
         input.type = 'file'
         input.setAttribute('multiple','')
+        input.classList.add('droppable')
         dropArea.appendChild(input)
-
+        
         div.appendChild(dropArea)
         
+
         const previewArea = document.createElement('div')
-        previewArea.classList.add('img-preview')
-        previewArea.innerHTML = '<h4>No hay imagenes cargadas</h4>'
+        previewArea.classList.add('img-preview','droppable')
+        previewArea.innerHTML = '<h4 class="droppable">No hay imagenes cargadas</h4>'
         previewArea.addEventListener('click', ()=> console.log('click pre'))
         div.append(previewArea)
     
+
+        document.documentElement.addEventListener('dragover', e=>{
+            e.preventDefault();
+            let cursor = "none";
+            if(e.target.classList.contains("droppable"))
+                cursor = "move";
+            e.dataTransfer.dropEffect = cursor;
+        });
+        document.addEventListener("dragover",function(e){
+            e.preventDefault();
+            e.stopPropagation();
+        });
+        document.addEventListener("drop",function(e){
+            e.preventDefault();
+            e.stopPropagation();
+        });
 
     }
     
